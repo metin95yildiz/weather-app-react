0bfb6c09e6eeea11e172af86b3e4d111d0f07082
metin95yildiz <metin_y1995@hotmail.com>
2022-02-25 18:57:28 +0300

    Implemented App js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Weather App</h1>
       </header>
+      <Content />
+      <footer>
+        <p>This is Weather App</p>
+      </footer>
     </div>
   );
 }
