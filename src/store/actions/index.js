010d4d3de3b2ba9214f0cd31a94c15eb4ff2acbe
metin95yildiz <metin_y1995@hotmail.com>
2022-02-25 22:22:30 +0300

    Created removeCity action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,3 +4,7 @@ import * as ACTION from "./actionTypes";
 export function addCity(cityName) {
     return { type: ACTION.ADD_CITY, cityName}
 }
+
+export function removeCity(cityName) {
+    return { type: ACTION.REMOVE_CITY, cityName }
+}
