1802b2d9e5ce677283f7f42487c857feea3aa80e
metin95yildiz <metin_y1995@hotmail.com>
2022-02-25 22:23:37 +0300

    Implemented CityList component

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchCity from "../SearchCity";
 import Time from "../Time";
+import CityList from "../CityList";
 import "./Content.scss";
 
 export default function Content() {
@@ -10,6 +11,7 @@ export default function Content() {
         <div className="content">
             <Time />
             <SearchCity />
+            <CityList />
          </div>
     )
 }
