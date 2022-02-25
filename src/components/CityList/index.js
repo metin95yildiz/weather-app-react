9a7a5dfdd0a7c42fe5590d8a64ffdc0d8cf5d05a
metin95yildiz <metin_y1995@hotmail.com>
2022-02-25 22:20:01 +0300

    Created CityList component

---
+++ src/components/CityList/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import City from "../City";
+import { citiesDataMemo } from "../../store/selectors";
+import "./CityList.scss";
+
+export default function CityList() {
+    const cities = useSelector(citiesDataMemo);
+    return (
+        <div className="city-list">
+            {
+                Object.values(cities).map((city) => {
+                    return (
+                        <City city={city} key={city} />
+                    )
+                })
+            }
+        </div>
+    )
+}
